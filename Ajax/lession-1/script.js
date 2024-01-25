0f1d6cc24d0caef87cc55f10f6e3ad1dea61915d
mdsabir07 <sabiruli@gmail.com>
2024-01-25 18:57:13 +0600

    lession1 update

---
+++ Ajax/lession-1/script.js
@@ -0,0 +1,16 @@
+function loadContent() {
+    // Create a new request
+    const xhr = new XMLHttpRequest();
+
+    // What to do when response arrives
+    xhr.onload = function() {
+        const container = document.getElementById('lession1');
+        lession1.innerHTML = xhr.responseText;
+    }
+
+    // prepare request methods: GET, POST, PUT, PATCH, DELETE, OPTIONS
+    xhr.open("GET", "./data/data.txt");
+
+    // Request send
+    xhr.send();
+}
